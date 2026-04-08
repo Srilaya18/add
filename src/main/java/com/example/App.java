2da0ae6823ba754cabac6e36f76a9c9c7fcee918
Srilaya18 <sriaya.m2023@vitstudent.ac.in>
2026-04-08 19:29:04 +0530

    first

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class App {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        App obj = new App();
+        System.out.println("Result = " + obj.add(5, 3));
+    }
+}
